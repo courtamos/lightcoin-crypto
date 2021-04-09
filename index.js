a266eb5d43f154737481d0e1b2f304d2c78895c5
Courtney Amos <c.amos@shaw.ca>
2021-04-09 14:10:30 -0700

    practice using OOP to implement logical in a 'banking' app

--- index.js
+++ index.js
@@ -1,29 +1,80 @@
-let balance = 500.00;
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
 
-class Withdrawal {
+  get balance() {
+    let balance = 0;
+    for (const transaction of this.transactions) {
+      balance += transaction.value;
+    }
+    return balance;
 
-  constructor(amount) {
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+}
+
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
-
   commit() {
-    balance -= this.amount;
+    if (!this.isAllowed()) {
+      console.log(`Insufficent Funds! You're current balance is $ ${myAccount.balance}`);
+      return false;
+    }
+    this.time = new Date();
+    this.account.addTransaction(this);
+    return true;
+
+  }
+}
+
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
   }
 
+  isAllowed() {
+    return true;
+  }
+}
+
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount;
+  }
+
+  isAllowed() {
+    return (this.account.balance - this.amount >= 0);
+  }
 }
 
 
 
-
 // DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account("court");
 
-t1 = new Withdrawal(50.25);
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(120.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(50.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+const t3 = new Deposit(2000.00, myAccount);
+t3.commit();
+
+const t4 = new Withdrawal(2050.00, myAccount);
+t4.commit();
+
+const t5 = new Withdrawal(100.00, myAccount);
+t5.commit();
+
+console.log('Ending Balance:', myAccount.balance);
